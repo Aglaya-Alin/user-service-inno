7a4b5755793c083cece375fda634af9be845ab8f
Aglaya-Alin <aglajaalin@gmail.com>
2026-05-22 00:03:10 +0300

    Initial commit

---
+++ src/main/java/com/example/user_service_inno/UserServiceInnoApplication.java
@@ -0,0 +1,13 @@
+package com.example.user_service_inno;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserServiceInnoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserServiceInnoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/user_service_inno/UserServiceInnoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.user_service_inno;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserServiceInnoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
